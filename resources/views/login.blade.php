5064aee4f35014ea0b6d73e3e00e19ebfe4d1bfb
pretohostdev <projectos@dev.petrohost.ao>
2024-04-18 08:50:01 +0100

    Novas atualizações

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,3 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <title>Login</title>
 
+    <style>
+        .container-fluid{
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .content{
+            width: 40%;
+            height: 90%;
+        }
+    </style>
+</head>
+<body>
 
-Login
+    <div class="container-fluid bg-secondary">
+        <div class="content bg-light">
+            <h3>Estou funcionando com Bootstrap 4</h3>
+        </div>
+    </div>
+
+    
+</body>
+</html>
